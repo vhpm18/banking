9facac6a3ba7f950954c1ed980bcfed59789cd68
VictorH <vhpm18@gmail.com>
2024-06-15 11:57:11 -0400

    implement inicial file and folder structute

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SigIn = () => {
+  return (
+    <div>SigIn</div>
+  )
+}
+
+export default SigIn
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SigUp = () => {
+  return (
+    <div>SigUp</div>
+  )
+}
+
+export default SigUp
